c9c22488efdbddf220d9ac72ddff6d71729590ae
Dricmoy <dricbhattacharjee@gmail.com>
2026-01-21 15:53:57 -0700

    user model defined

---
+++ backend/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey, Enum
+from sqlalchemy.orm import relationship
+import enum
+from database import Base
+from datetime import datetime
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+
